b90952eff51d1a45b246a684e014e37512f35bb2
RMS <rogelio.samson@gmail.com>
2019-07-14 18:02:37 +0800

    Initial Codes

---
+++ rms-config-server/src/main/java/com/rogersamson/RmsConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.rogersamson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmsConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmsConfigServerApplication.class, args);
+	}
+
+}
