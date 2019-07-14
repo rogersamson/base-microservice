546ea77ae9abcca61ef51962a1fa6bbe9f387507
RMS <rogelio.samson@gmail.com>
2019-07-14 18:18:14 +0800

    Naming Server

---
+++ rms-naming-server/src/main/java/com/rogersamson/RmsNamingServerApplication.java
@@ -0,0 +1,13 @@
+package com.rogersamson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmsNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmsNamingServerApplication.class, args);
+	}
+
+}
