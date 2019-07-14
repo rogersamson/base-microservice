35c0e47b9b93fd114171e6106bcf0622d42b1060
RMS <rogelio.samson@gmail.com>
2019-07-14 18:14:18 +0800

    Initial Base Micro

---
+++ rms-micro-base/src/main/java/com/rogersamson/RmsMicroBaseApplication.java
@@ -0,0 +1,13 @@
+package com.rogersamson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmsMicroBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmsMicroBaseApplication.class, args);
+	}
+
+}
---
+++ rms-micro-base/src/test/java/com/rogersamson/RmsMicroBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogersamson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmsMicroBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
