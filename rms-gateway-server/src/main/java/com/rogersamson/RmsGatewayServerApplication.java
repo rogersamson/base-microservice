e39dd34154a940a5bf3e9b96db63cf382f1c2c75
RMS <rogelio.samson@gmail.com>
2019-07-14 18:20:01 +0800

    Base Gateway Server

---
+++ rms-gateway-server/src/main/java/com/rogersamson/RmsGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.rogersamson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmsGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmsGatewayServerApplication.class, args);
+	}
+
+}
